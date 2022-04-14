34586b0e7b55bad55bf1eda27831c3dd54c5fa78
Abdurrahim <abdurahim.abdo.zer@gmail.com>
2022-04-14 05:20:07 +0300

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -4,7 +4,6 @@ from pytube import Playlist
 from os import mkdir,listdir,rename,system
 from os.path import splitext
 from sys import platform
-from renamefile import rename_file
 
 def finish():
   print("\tVideo downloaded.")
@@ -12,99 +11,137 @@ def finish():
 def finished():
   print("Video downloaded.")
 
-def altagec():
+def takedownline():
   print()
 
-def download(option,sesvar=0):
+def download(option,voice_on=0):
   if "playlist" in option:
     playlist = Playlist(option)
     if "Downloaded Videos" not in listdir():
         mkdir("Downloaded Videos")
     if playlist.title not in listdir("Downloaded Videos"):
-      mkdir("Downloaded Videos" +"\\"+ str(playlist.title))
+        mkdir("Downloaded Videos" +"\\"+ str(playlist.title))
     downloadpath = "Downloaded Videos" +"\\"+ playlist.title
     i = 0
-    if sesvar == 0:
+    if voice_on == 0:
       for yt in playlist.videos:
-        vid = yt.streams.get_highest_resolution().download(output_path=downloadpath)
-        i += 1
-        if len(yt.title) <= 35:
-          yazi = str(i) + ". \t" + yt.title
-          print(yazi + "." * (45 - len(yazi)),end="")
-        elif len(yt.title) > 35:
-          yazi = str(i) + ". \t" + yt.title[:35]
-          print(yazi + "." * (45 - len(yazi)),end="")
-        b,e = path.splitext(vid)
-        rename(vid,rename_file(b) + ".mp4")
-        yt.register_on_complete_callback(finish())
-    elif sesvar == 1:
+        try:
+          vid = yt.streams.get_highest_resolution().download(output_path=downloadpath)
+          yt_title = yt.title
+          i += 1
+          if len(yt_title) <= 35:
+            print(option)
+            yt_title = str(i) + ". \t" + yt_title
+            print(yt_title + "." * (45 - len(yt_title)))
+          elif len(yt_title) > 35:
+            print(option)
+            yt_title = str(i) + ". \t" + yt_title[:35]
+            print(yt_title + "." * (45 - len(yt_title)))
+          b,e = splitext(vid)
+          rename(vid,b + ".mp4")
+          yt.register_on_complete_callback(finish())
+          print("-"*50)
+        except:
+          pass
+    elif voice_on == 1:
       for yt in playlist.videos:
-        vid = yt.streams.filter(only_audio=True).first().download(output_path=downloadpath)
-        i += 1
-        if len(yt.title) <= 35:
-          yazi = str(i) + ". \t" + yt.title
-          print(yazi + "." * (45 - len(yazi)),end="")
-        elif len(yt.title) > 35:
-          yazi = str(i) + ". \t" + yt.title[:35]
-          print(yazi + "." * (45 - len(yazi)),end="")
-        b,e = path.splitext(vid)
-        rename(vid,rename_file(b) + ".mp3")
-        yt.register_on_complete_callback(finish())
+        try:
+          vid = yt.streams.filter(only_audio=True).first().download(output_path=downloadpath)
+          yt_title = yt.title
+          i += 1
+          if len(yt_title) <= 35:
+            print(option)
+            yt_title = str(i) + ". \t" + yt_title
+            print(yt_title + "." * (45 - len(yt_title)))
+          elif len(yt_title) > 35:
+            print(option)
+            yt_title = str(i) + ". \t" + yt_title[:35]
+            print(yt_title + "." * (45 - len(yt_title)))
+          b,e = splitext(vid)
+          rename(vid,b + ".mp3")
+          yt.register_on_complete_callback(finish())
+          print("-"*50)
+        except:
+          pass
     
   elif "watch?v" in option and "playlist" not in option:
     yt = YouTube(option, on_progress_callback=on_progress)
+    yt_title = yt.title
     if "Downloaded Videos" not in listdir():
       mkdir("Downloaded Videos")
     downloadpath = "Downloaded Videos"
-    if sesvar == 0:
+    if voice_on == 0:
       vid = yt.streams.get_highest_resolution().download(output_path=downloadpath)
-      b,e = path.splitext(vid)
-      rename(vid,rename_file(b) + ".mp4")
-      yt.register_on_complete_callback(altagec())
-      finished() 
-    elif sesvar == 1:
+      if len(yt_title) <= 35:
+        print(option)
+        print(yt_title + "." * (45 - len(yt)))
+      elif len(yt_title) > 35:
+        print(option)
+        print(yt_title[:35] + "." * (45 - len(yt_title)))
+      b,e = splitext(vid)
+      rename(vid,b + ".mp4")
+      yt.register_on_complete_callback(takedownline())
+      finished()
+    elif voice_on == 1:
       vid = yt.streams.filter(only_audio=True).first().download(output_path=downloadpath)
-      b,e = path.splitext(vid)
-      rename(vid,rename_file(b) + ".mp3")
-      yt.register_on_complete_callback(altagec())
+      if len(yt_title) <= 35:
+        print(option)
+        print(yt_title + "." * (45 - len(yt_title)))
+      elif len(yt_title) > 35:
+        print(option)
+        print(yt_title[:35] + "." * (45 - len(yt_title)))
+      b,e = splitext(vid)
+      rename(vid,b + ".mp3")
+      yt.register_on_complete_callback(takedownline())
       finished()
   else:
     print("Try a YouTube URL")
 
 if __name__ == '__main__':
   while True:
-    isitafile = input("Is there a file I should take care of ? y/n:")
+    isitafile = input("Is there a file I should take care of ? y/n: ").strip()
     if isitafile.lower() == "y":
       urls = []
-      wherethefile = input("Where is the file: ")
+      wherethefile = input("Where is the file: ").strip()
       with open(wherethefile,"r",encoding="utf8") as f:
         urls = [str(x).strip() for x in f.readlines() if "youtube.com/watch" in str(x) or "youtube.com/playlist" in str(x)]
-      sesvar   = input("Do you want to download only audio? y/n: ")
-      if sesvar.lower() == "y":
+      voice_on   = input("Do you want to download only audio? y/n: ").strip()
+      print("-"*50)
+      if voice_on.lower() == "y":
         for x in urls:
-          download(x,1)
-      elif sesvar.lower() == "n":
+          try:
+            download(x,1)
+            print("-"*50)
+          except:
+            pass
+      elif voice_on.lower() == "n":
         for x in urls:
-          download(x,0)
+          try:
+            download(x,0)
+            print("-"*50)
+          except:
+            pass
       else:
         print("Please enter a legit answer!!!  y or n")
         continue
     elif isitafile.lower() == "n":
       option   = input("Please Enter URL: ")
-      sesvar   = input("Do you want to download only audio? y/n: ")
-      if sesvar.lower() == "y":
+      voice_on   = input("Do you want to download only audio? y/n: ")
+      print("-"*50)
+      if voice_on.lower() == "y":
         download(option,1)
-      elif sesvar.lower() == "n":
+      elif voice_on.lower() == "n":
         download(option,0)
       else:
         print("Please enter a legit answer!!!  y or n")
         continue
     else:
-      print("Come onn mannn type a letter what I want!!")
+      print("Come on man type a letter what I want!!")
       continue
     while True:
       ask = input("Do you want to download sth else?(y/n): ")
       if ask.lower() == "y":
+        print("-"*50)
         break
       elif ask.lower() == "n":
         exit()
@@ -117,7 +154,7 @@ if __name__ == '__main__':
         continue
   if platform == "win32":
     system("cls")
-    system("exit")
+    exit()
   else:
     system("clear")
-    system("exit")
+    exit()
